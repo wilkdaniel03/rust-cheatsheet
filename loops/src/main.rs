7ccb27826c8effa9c57d3544dce8f0f4a7355cd3
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 17:45:52 +0200

    [loops] While loop.

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // Loops in rust
+    while_loop();
+}
+
+fn while_loop() {
+    // I think, while is the simplest one 
+    let mut x = 0;
+    while x < 10 {
+        println!("{}", x);
+        x += 1;
+    }
 }
