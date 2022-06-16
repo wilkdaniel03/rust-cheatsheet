dca7ede7f57e9215096a01ed77e8ceab3af5cca1
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 16:33:42 +0200

    [variables] Mutable variables.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    immutable_variable();
+    // immutable_variable();
+    mutable_variable();
 }
+
+#[allow(dead_code)]
 
 fn immutable_variable() {
     // Declare a immutable variable
@@ -10,4 +13,12 @@ fn immutable_variable() {
     // println!("The value of x is {}", x);
     // That would return an error because
     // we cannot edit the x variable.
+}
+
+fn mutable_variable() {
+    // Declare a mutable variable
+    let mut x = 5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
 }
