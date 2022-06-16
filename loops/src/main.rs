2267f0754f044d859906e199a262c6bcc1bfd42a
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 17:52:52 +0200

    [loops] While let loop.

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,13 +1,25 @@
 fn main() {
     // Loops in rust
-    while_loop();
+    // while_loop();
+    while_let_loop();
 }
 
+#[allow(dead_code)]
 fn while_loop() {
     // I think, while is the simplest one 
     let mut x = 0;
     while x < 10 {
         println!("{}", x);
         x += 1;
+    }
+}
+
+fn while_let_loop() {
+    // There is also somethink called while let loop
+    // this loop will iterate as long as
+    // nums.pop() won't return any error
+    let mut nums = vec![1, 2, 3];
+    while let Some(x) = nums.pop() {
+        println!("{}", x);
     }
 }
