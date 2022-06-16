c086f1a98a66f43a81906a0230bebde83259a23d
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 16:40:22 +0200

    [variables] Constants.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     // immutable_variable();
-    mutable_variable();
+    // mutable_variable();
+    constant();
 }
 
 #[allow(dead_code)]
-
 fn immutable_variable() {
     // Declare a immutable variable
     let x = 5;
@@ -15,10 +15,19 @@ fn immutable_variable() {
     // we cannot edit the x variable.
 }
 
+#[allow(dead_code)]
 fn mutable_variable() {
     // Declare a mutable variable
     let mut x = 5;
     println!("The value of x is {}", x);
     x = 6;
     println!("The value of x is {}", x);
+}
+
+#[allow(dead_code)]
+fn constant() {
+    // Declare a constant
+    const X: i32 = 5;
+    // Constants have to have declared type and hardcoded value
+    // Constants should be named in uppercase
 }
