c3d70970a2c3c53cd7a14d6fedcc178ffb5bc81d
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 17:16:45 +0200

    [types] Integer types.

--- types/src/main.rs
+++ types/src/main.rs
@@ -3,5 +3,20 @@
 // every variable's type at compile time
 
 fn main() {
-    println!("Hello, world!");
+    scalar_types();
+}
+
+#[allow(unused_variables)]
+fn scalar_types() {
+    // Scalar types - represent single values
+    // Integer types
+    let int: i8 = 100; /* -128 - 127 | 0 - 255 */ let int: u8 = 200;
+    let int: i16 = 100; /* -32768 - 32767 | 0 - 65535 */ let int: u16 = 200;
+    let int: i32 = 100; /* ~-2Bil - ~2Bil | 0 - ~4Bil */ let int: u32 = 200;
+    let int: i64 = 100; /* The really big one */ let int: u64 = 200;
+    let int: i128 = 100; /* A LOT */ let int: u128 = 200;
+    // The think is we certainly should use 32/64 bits depending on system arch
+    // for big numbers or just use that
+    let int: isize = 100; let int: usize = 200;
+    // 128 bits are certainly used in cryptography
 }
