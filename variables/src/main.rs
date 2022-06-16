66564ffea46758e488e255056878cb420f7d7953
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 16:28:27 +0200

    [variables] Immutable variables.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    immutable_variable();
+}
+
+fn immutable_variable() {
+    // Declare a immutable variable
+    let x = 5;
+    println!("The value of x is {}", x);
+    // x = 6;
+    // println!("The value of x is {}", x);
+    // That would return an error because
+    // we cannot edit the x variable.
 }
